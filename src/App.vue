ccc657535a698279277d7d48421c9dd4273090f4
Ruslanchi1 <102151248+Ruslanchi1@users.noreply.github.com>
2022-05-22 15:24:23 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,111 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="calc">
+      <div class="calc__screen">
+        <div>{{calculatorValue || 0}}</div>
+        <input v-model="calculatorValue" type="text" class="screen"/>
+      </div>
+      <div class="calc__but">
+        <div class="buttons">
+          <button 
+            class="btn" 
+            :class="{'zero': '0'.includes(n)}"
+            v-for="n in calculatorElements" 
+            :key="n"
+            @click="action(n)"
+          >
+            {{ n }}
+          </button>
+          <!-- <div class="btn C"><button>C</button></div>
+                <div class="btn +/-"> <button>+/-</button></div>
+                <div class="btn %"> <button> %</button></div>
+                <div class="btn /"> <button>/</button></div>
+                <div class="btn 7"> <button>7</button></div>
+                <div class="btn 8"> <button>8</button></div>
+                <div class="btn 9"> <button>9</button></div>
+                <div class="btn X"> <button>X</button></div>
+                <div class="btn 4"> <button>4</button></div>
+                <div class="btn 5"> <button>5</button></div>
+                <div class="btn 6"> <button>6</button></div>
+                <div class="btn -"> <button>-</button></div>
+                <div class="btn 1"> <button>1</button></div>
+                <div class="btn 2"> <button>2</button></div>
+                <div class="btn 3"> <button>3</button></div>
+                <div class="btn +"> <button>+</button></div>
+                <div class="btn zero"> <button>0</button></div>
+                <div class="btn ,"> <button>,</button></div>
+                <div class="btn ="> <button>=</button></div> -->
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      calculatorValue: '',
+      calculatorElements: [
+        "C",
+        "+/-",
+        "%",
+        "/",
+        "7",
+        "8",
+        "9",
+        "*",
+        "4",
+        "5",
+        "6",
+        "-",
+        "1",
+        "2",
+        "3",
+        "+",
+        "0",
+        ",",
+        "=",
+      ],
+      operator: null,
+      previousCalculatorValue: ''
+    }
+  },
+  methods: {
+    action(n) {
+      if (!isNaN(n) || n === ',') {
+        this.calculatorValue += n + ''
+      }
+
+      if (n === 'C') {
+        this.calculatorValue = ''
+      }
+
+      if (n === '%') {
+        this.calculatorValue = this.calculatorValue / 100 + ''
+        console.log(this.calculatorValue)
+      }
+
+      if (n === '+/-') {
+        this.calculatorValue = this.calculatorValue * -1 + ''
+      }
+
+      if ( ['/','*','-','+'].includes(n)) {
+        this.operator = n
+        this.previousCalculatorValue = this.calculatorValue
+        this.calculatorValue = ''
+      }
+
+      if (n === '=') {
+        this.calculatorValue = eval(this.previousCalculatorValue + this.operator + this.calculatorValue)
+        
+        this.previousCalculatorValue = '';
+        this.operator = null;
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
